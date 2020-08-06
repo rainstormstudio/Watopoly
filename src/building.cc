7261cda42df93eb9cd690c47082e73571ba74287
DiamondSlug <rainstormstudio@yahoo.com>
2020-08-06 11:14:11 -0400

    finish Building

---
+++ src/building.cc
@@ -0,0 +1,18 @@
+#include "building.h"
+
+Building::Building(Vec2 coord, std::string name, unsigned int cost) 
+    : Square(coord, name), cost{cost} {
+        owner = nullptr;
+}
+
+std::shared_ptr<Player> Building::getOwner() const { return owner; }
+
+void Building::setOwner(std::shared_ptr<Player> player) {
+    owner = player;
+}
+
+unsigned int Building::getCost() const { return cost; }
+
+void Building::update(std::shared_ptr<Player> player) {}
+
+void Building::render(std::shared_ptr<Graphics> gfx) {}
